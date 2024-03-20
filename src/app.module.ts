bc2c8c4ea6a3269d7d11495b11a8c21b1845a49d
Antonis Mavrakis <antonis.mavrak@gmail.com>
2024-03-20 12:21:50 +0200

    initial commit
    Signed-off-by: Antonis Mavrakis <antonis.mavrak@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { UsersModule } from "./users/users.module";
+import { CommentsModule } from "./comments/comments.module";
+import { MongooseModule } from "@nestjs/mongoose";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+const MONGO_USER = process.env.MONGO_USERNAME;
+const MONGO_PASS = process.env.MONGO_PASSWORD;
+const MONGO_CLUSTER = process.env.MONGO_CLUSTER;
+const MONGO_CLOUD_CODE = process.env.MONGO_CLOUD_CODE;
+const MONGO_DB_THREADS = process.env.MONGO_DATABASE_THREADS;
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    CommentsModule,
+    MongooseModule.forRoot(`mongodb+srv://${MONGO_USER}:${MONGO_PASS}@${MONGO_CLUSTER}.${MONGO_CLOUD_CODE}.mongodb.net/${MONGO_DB_THREADS}?retryWrites=true&w=majority`)],
   controllers: [AppController],
   providers: [AppService]
 })
